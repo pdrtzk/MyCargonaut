798f2f2b691f7ecd7c0ebec992ed01383ae2eef3
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2020-12-16 17:06:14 +0100

    Ignores unused test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('MyCargonaut');
   });
 
-  it('should render title', () => {
+  xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
