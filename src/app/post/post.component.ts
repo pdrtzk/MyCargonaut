5417a967b5b6ea96116ad31a6969cab59d0379aa
ChEb <christiana.eberle@mni.thm.de>
2020-12-30 12:17:57 +0100

    dynamic star rating WIP

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Post } from 'src/shared/post.model';
+import {Component, OnInit, Input} from '@angular/core';
+import {Post} from 'src/shared/post.model';
 
 @Component({
   selector: 'app-post',
@@ -8,9 +8,13 @@ import { Post } from 'src/shared/post.model';
 })
 export class PostComponent implements OnInit {
   @Input() post: Post;
-  public loremIpsum: string = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
-
+  public loremIpsum: string = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+  public starRatingString = ''; // todo use material icons
 
   ngOnInit(): void {
+    let rating = 4; // todo
+    for (let i = 0; i < rating; i++) {
+      this.starRatingString += '⭐';
+    }
   }
 }
