627445d1b07b13f1be417cc89f603ad674b083a0
bift85 <britta.fichtl@mni.thm.de>
2020-12-31 15:43:12 +0100

    started header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
 import {AuthGuard} from './auth';
 import {AppComponent} from './app.component';
 
 export const routes: Routes = [
-   {path: '', component: AppComponent, /*canActivate: [AuthGuard]*/},
+   {path: '', component: HomeComponent, /*canActivate: [AuthGuard]*/},
   // {path: 'login', component: LoginComponent},
   // {path: 'register', component: RegisterComponent},
   {path: '**', redirectTo: ''}
