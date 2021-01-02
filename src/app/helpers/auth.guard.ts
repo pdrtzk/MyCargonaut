98d0d80da4c0088afc72fb4289f7cbd7dda55132
Dxree <pia-doreen.ritzke@mni.thm.de>
2021-01-02 19:02:03 +0100

    Fixes auth bug.

--- src/app/helpers/auth.guard.ts
+++ src/app/helpers/auth.guard.ts
@@ -7,42 +7,47 @@ import {of} from 'rxjs';
 import {MatDialog} from '@angular/material/dialog';
 import {AppComponent} from '../app.component';
 import {AccountComponent} from '../components/account/account.component';
+import {AccountService} from '../services/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(/* readonly auth: AuthService, */ private router: Router, private dialog: MatDialog) {
+  constructor(readonly accountService: AccountService, private router: Router, private dialog: MatDialog) {
   }
 
   /** Returns true whenever the user is authenticated */
   get authenticated() {
-    return false; // this.auth.authenticated;
+    return !!this.accountService.user;
   }
 
   /** Returns the current authenticated user id */
   get userId() {
-    return false; // this.auth.userId;
+    return this.accountService.user.id;
   }
 
   // Implements single route user authentication guarding
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    /*
-    // const user = this.accountService.userValue;
+    // TODO: Hintergrund "verstecken", wenn noch nicht angemeldet...
+    // TODO: Nach Logout direkt wieder zu Home zurück
+    const user = this.accountService.user;
     if (user) {
       // authorised so return true
       return true;
     }
 
     // not logged in so redirect to login page with the return url
-     */
+
     // this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url }});
-    console.log('auth guard says hello');
+    console.log('auth guard opens pop up on: ' + state.url);
     const dialogRef = AccountComponent.openDialog(this.dialog);
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('closed: ' + result);
+      if (!result) {
+        this.router.navigate([state.url]).then();
+      }
     });
     return false;
 
