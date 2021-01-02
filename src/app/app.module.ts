e1ea1df1861d8dd03b1a23e17c6221946c9f4ada
ChEb <christiana.eberle@mni.thm.de>
2021-01-02 12:06:31 +0100

    add star rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { PostDisplayCardComponent } from './post/post-display-card/post-display-card.component';
+import { StarRatingComponent } from './post/star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    PostDisplayCardComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/post/star-rating/star-rating.component.ts
+++ src/app/post/star-rating/star-rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-star-rating',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarRatingComponent implements OnInit {
 
+  @Input() starCount: number;
   constructor() { }
 
   ngOnInit(): void {
