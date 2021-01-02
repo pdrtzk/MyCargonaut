9aac4e4e06656d9851f860d9d87a45914b01c3c1
ChEb <christiana.eberle@mni.thm.de>
2021-01-02 15:18:26 +0100

    add Components to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostDisplayCardComponent } from './post/post-display-card/post-display-card.component';
 import { StarRatingComponent } from './post/star-rating/star-rating.component';
+import {PostVehicleTypeIconComponent} from './post/post-vehicle-type-icon/post-vehicle-type-icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
     PostDisplayCardComponent,
-    StarRatingComponent
+    StarRatingComponent,
+    PostVehicleTypeIconComponent
   ],
   imports: [
     BrowserModule,
