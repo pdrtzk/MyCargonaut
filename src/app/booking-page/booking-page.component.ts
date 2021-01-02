9487f0e65b4641799d363ef6ff16eaeadce7b531
ChEb <christiana.eberle@mni.thm.de>
2021-01-02 15:02:14 +0100

    add booking page component

---
+++ src/app/booking-page/booking-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-page',
+  templateUrl: './booking-page.component.html',
+  styleUrls: ['./booking-page.component.css']
+})
+export class BookingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
