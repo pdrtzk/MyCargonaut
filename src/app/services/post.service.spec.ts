e67fc50f96007a8f203c512939c5ffa9cdfe1d10
Leona Kirchem <leona.kirchem@mni.thm.de>
2021-01-03 15:40:55 +0100

    lint error

--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -1,5 +1,5 @@
 import {TestBed} from '@angular/core/testing';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from '@angular/common/http';
 
 import {PostService} from './post.service';
 
